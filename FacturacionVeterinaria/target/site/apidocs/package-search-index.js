7c8ffcc5ebd315e8f1ea87acc71d114c166ef81e
Felixphz <105894241+Felixphz@users.noreply.github.com>
2023-11-17 19:25:32 -0500

    change
    ubicacion en los archivos

---
+++ FacturacionVeterinaria/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.mycompany.facturacionveterinaria"},{"l":"Control"},{"l":"DataBase"},{"l":"Model"},{"l":"View"}];updateSearchResults();
